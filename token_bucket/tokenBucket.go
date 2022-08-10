46208eebc7e095970fe1dd2c2f9b2cda7ae243e5
Eval EXEC <execvy@gmail.com>
2022-08-10 22:56:22 +0800

    Impl TokenBucket

---
+++ token_bucket/tokenBucket.go
@@ -0,0 +1,54 @@
+package token_bucket
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+)
+
+func New(ctx context.Context, cancel context.CancelFunc, qps uint64) *TokenBucket {
+	return &TokenBucket{
+		ctx:    ctx,
+		cancel: cancel,
+		Qps:    qps,
+		tokens: qps,
+	}
+
+}
+
+type TokenBucket struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	Qps    uint64
+	tokens uint64
+}
+
+func (r *TokenBucket) addTokens() {
+
+	tk := time.NewTicker(time.Second / time.Duration(r.Qps))
+	for {
+		select {
+		case <-tk.C:
+			atomic.AddUint64(&r.tokens, 1)
+		case <-r.ctx.Done():
+			return
+		}
+	}
+
+}
+
+func (r *TokenBucket) GetToken() bool {
+retry:
+	tokens := atomic.LoadUint64(&r.tokens)
+	if tokens > 0 {
+		if !atomic.CompareAndSwapUint64(&r.tokens, tokens, tokens-1) {
+			goto retry
+		}
+		return true
+	}
+	return false
+}
+
+func (r *TokenBucket) Close() {
+	r.cancel()
+}
